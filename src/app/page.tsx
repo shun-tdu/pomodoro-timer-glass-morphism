8ad505cc91513ff0e67ca074a928e1b23d3198ed
shun-tdu <25kmh15@ms.dendai.ac.jp>
2026-05-13 13:03:24 +0900

    feat:作業と休憩を切り替える機能を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,46 @@ import { useState, useEffect } from "react";
 import { GlassButton } from "@/componens/GlassButton";
 import Image from "next/image";
 
-export default function Home() {
-  const WORK_TIME: number = 2 * 60;
-  const REST_TIME: number = 1 * 60;
+type TimerMode = "idle" | "work" | "rest";
 
+const MODE_DISPLAY_TEXT: Record<TimerMode, string> = {
+  idle: "Ready?",
+  work: "Focus!",
+  rest: "Rest",
+};
+
+const WORK_TIME: number = 2 * 10;
+const REST_TIME: number = 1 * 10;
+
+// 時刻を受け取り、表示する
+function GetFormattedTime(time_minutes: number) {
+  const minutes: number = Math.floor(time_minutes / 60);
+  const seconds: number = time_minutes % 60;
+  const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  return formattedTime;
+}
+
+export default function Home() {
   const [timeText, setTimeText] = useState(() => GetFormattedTime(WORK_TIME));
+  const [timerMode, setTimerMode] = useState<TimerMode>("idle");
   const [isCount, setIsCount] = useState(false);
-  const [isRest, setIsRest] = useState(false);
   const [startTime, setStartTime] = useState(0);
   const [accumulatedTime, setAccumulatedTime] = useState(0);
 
+  // カウント可能な状態の時にタイマーを開始
   function StartTimer() {
+    if (timerMode === "idle") return;
     if (isCount) return;
+
     setStartTime(Date.now());
     setIsCount(true);
   }
 
+  // カウント可能な状態の時にタイマーを一時停止
   function StopTimer() {
+    if (timerMode === "idle") return;
     if (!isCount) return;
+
     setIsCount(false);
     setAccumulatedTime((prev) => prev + Date.now() - startTime);
   }
@@ -32,30 +54,118 @@ export default function Home() {
     setIsCount(false);
   }
 
-  // 時刻を受け取り、表示する
-  function GetFormattedTime(time_minutes: number) {
-    const minutes: number = Math.floor(time_minutes / 60);
-    const seconds: number = time_minutes % 60;
-    const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
-    return formattedTime;
+  function EnterWork() {
+    if (timerMode === "work") return;
+
+    // カウント開始時刻記録
+    ResetTimer();
+    if (!isCount) setIsCount(true);
+    setStartTime(Date.now());
+
+    // 状態遷移
+    setTimerMode("work");
   }
 
-  function CalculateTimer() {
-    // 経過時間経過処理
-    const now = Date.now();
-    const totalMs = accumulatedTime + (now - startTime);
-    const remainingTime: number = WORK_TIME - Math.floor(totalMs / 1000);
+  function EnterRest() {
+    if (timerMode === "rest") return;
 
-    // 残り時間判定
-    if (remainingTime <= 0) {
-      // 休憩開始処理
+    // カウント開始時刻記録
+    ResetTimer();
+    if (!isCount) setIsCount(true);
+    setStartTime(Date.now());
+
+    // 状態遷移
+    setTimerMode("rest");
+  }
+
+  function EnterIdle() {
+    if (timerMode === "idle") return;
+
+    // タイマーリセット
+    ResetTimer();
+
+    // 状態遷移
+    setTimerMode("idle");
+  }
+
+  function HandleMainButtonAction() {
+    switch (timerMode) {
+      case "idle":
+        EnterWork();
+        break;
+      case "work":
+        if (isCount) {
+          StopTimer();
+        } else {
+          StartTimer();
+        }
+        break;
+      case "rest":
+        if (isCount) {
+          StopTimer();
+        } else {
+          StartTimer();
+        }
+        break;
     }
+  }
 
-    // タイマー表示処理
-    setTimeText(GetFormattedTime(remainingTime));
+  function GetMainButtonText() {
+    let button_text: string = "";
+
+    switch (timerMode) {
+      case "idle":
+        button_text = "Start";
+        break;
+      case "work":
+        if (isCount) {
+          button_text = "Stop";
+          break;
+        } else {
+          button_text = "Resume";
+          break;
+        }
+      case "rest":
+        if (isCount) {
+          button_text = "Stop";
+          break;
+        } else {
+          button_text = "Resume";
+          break;
+        }
+    }
+    return button_text;
   }
 
   useEffect(() => {
+    // タイマー画面の状態変更を司る
+    function CalculateTimer() {
+      let time_criterion: number = WORK_TIME;
+
+      if (timerMode === "work") {
+        time_criterion = WORK_TIME;
+      } else if (timerMode === "rest") {
+        time_criterion = REST_TIME;
+      }
+
+      // 経過時間経過処理
+      const now = Date.now();
+      const totalMs = accumulatedTime + (now - startTime);
+      const remainingTime: number = time_criterion - Math.floor(totalMs / 1000);
+
+      // 残り時間判定
+      if (remainingTime <= 0) {
+        if (timerMode === "work") {
+          EnterRest();
+        } else if (timerMode === "rest") {
+          EnterWork();
+        }
+      }
+
+      // タイマー表示処理
+      setTimeText(GetFormattedTime(remainingTime));
+    }
+
     if (!isCount) return;
 
     const intervalID = setInterval(() => CalculateTimer(), 100);
@@ -63,7 +173,7 @@ export default function Home() {
     return () => {
       clearInterval(intervalID);
     };
-  }, [isCount, accumulatedTime, startTime]);
+  }, [isCount, accumulatedTime, startTime, timerMode]);
 
   return (
     <main className="relative min-h-screen flex flex-col items-center justify-center">
@@ -76,16 +186,16 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center gap-6">
         <h1 className="font-mono text-[42px] md:text-7xl">Pomodoro Timer</h1>
         <h1 className="font-mono text-6xl md:text-7xl">
-          {isRest ? "Rest" : "Focus!"}
+          {MODE_DISPLAY_TEXT[timerMode]}
         </h1>
 
         <h1 className="font-mono text-6xl tracking-widest">{timeText}</h1>
 
         <div className="flex flex-col justify-center items-center gap-6">
-          <GlassButton color="white" onClick={isCount ? StopTimer : StartTimer}>
-            {isCount ? "Stop" : "Start"}
+          <GlassButton color="white" onClick={HandleMainButtonAction}>
+            {GetMainButtonText()}
           </GlassButton>
-          <GlassButton color="white" onClick={ResetTimer}>
+          <GlassButton color="white" onClick={EnterIdle}>
             Reset
           </GlassButton>
         </div>
